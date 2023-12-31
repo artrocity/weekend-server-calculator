5bce51379ddaeaa1c9b6250e826c71601cfa4743
Ryan Barfield <cadium828@gmail.com>
2023-12-31 15:13:46 -0600

    updated client.js and index.html to allow post requests and display calc results on tbody

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -33,4 +33,41 @@ function renderCalculations(calculations) {
         });
 }
 
+// Add event listener for the buttons
+let selectedOperator = null;
+
+document.querySelectorAll('.operator').forEach(button => {
+    button.addEventListener('click', function() {
+        // Remove active class from all buttons
+        document.querySelectorAll('.operator').forEach(btn => btn.classList.remove('active'));
+        
+        // Add active class to the clicked button
+        this.classList.add('active');
+        selectedOperator = this.value;
+    });
+});
+
+// Get the values from the form
+document.getElementById('calculationForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the form from submitting traditionally
+
+    if (!selectedOperator) {
+        alert('Please select an operator');
+        return;
+    }
+
+    const numOne = parseFloat(document.getElementById('firstNumber').value);
+    const numTwo = parseFloat(document.getElementById('secondNumber').value);
+
+    submitCalculation(numOne, numTwo, selectedOperator);
+});
+
 // Make an axios post request and submit data to server to be calculated
+function submitCalculation(numOne, numTwo, operator) {
+    axios.post('/calculations', { numOne, numTwo, operator })
+        .then(() => {
+            // Fetch updated calculations after successful POST
+            getCalculations();
+        })
+        .catch(error => console.error('Error submitting calculation:', error));
+}
